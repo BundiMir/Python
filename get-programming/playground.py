70cc0ca730a3120e966bb82af9db06ca55634791
A.PBundil <A.PBundil@moravia.com>
2019-03-07 13:28:47 +0100

    convert-minutes + playground

--- get-programming/playground.py
+++ get-programming/playground.py
@@ -1,10 +1,19 @@
+# convert farenheit to celsius
 fahrenheit = 75
 celsius = (fahrenheit - 32) / 1.8
 print(int(celsius))
 
+# convert miles to km & meters
 miles = 5
 km = miles / 0.62137
 meters = 1000 * km
 print("Miles: " + str(miles))
 print("Km: " + str(round(km,4)))
 print("Meters: " + str(round(meters,4)))
+
+
+ikemsflqap
+pes 
+sap 
+pasik 
+
